b1f6ec67c73396072f7c13f3ea946522b7324066
electrikbox <electrikbox@icloud.com>
2023-12-18 12:25:36 +0100

    remove unecessary lines/update csv writer

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,38 +1,30 @@
 #!/usr/bin/python3
 """Script that save in csv infos from a given employee"""
 
+import csv
 import requests
 import sys
-import csv
 
 API_URL = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == '__main__':
-    # user info
-    user_request = requests.get(f'{API_URL}/users/{sys.argv[1]}')
-    user_data = user_request.json()
+    
+    USER_ID = sys.argv[1]
 
-    # todo info
-    todo_list_request = requests.get(f"{API_URL}/todos?userId={sys.argv[1]}")
-    todo_list_data = todo_list_request.json()
+    user = requests.get(f'{API_URL}/users/{USER_ID}').json()
+    todo_list = requests.get(f"{API_URL}/todos?userId={USER_ID}").json()
 
-    # completed todo
-    completed_tasks = [task for task in todo_list_data if task['completed']]
-
-    user_name = user_data["name"]
-    len_completed_tasks = len(completed_tasks)
-    total_todo = len(todo_list_data)
-
-    # save in csv file
-    csv_filename = f"{sys.argv[1]}.csv"
+    csv_filename = f"{USER_ID}.csv"
 
     with open(csv_filename, mode='w') as csv_file:
-        csv_writer = csv.writer(csv_file,
-                                delimiter=',',
-                                quotechar='"',
-                                quoting=csv.QUOTE_ALL)
-        for task in todo_list_data:
-            csv_writer.writerow([sys.argv[1],
-                                 user_name,
-                                 task['completed'],
-                                 task['title']])
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        
+        for task in todo_list:
+            csv_writer.writerow([
+                user['id'],
+                user['username'],
+                task['completed'],
+                task['title']
+            ])
+
+    print(f"Data has been exported to {USER_ID}.csv")
