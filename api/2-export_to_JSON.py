2d0642c4581cf1d6d184a9f66b2b21f80f6bb5f5
electrikbox <electrikbox@icloud.com>
2023-12-18 12:35:01 +0100

    save in json file

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script that save in csv infos from a given employee"""
+
+import json
+import requests
+import sys
+
+API_URL = 'https://jsonplaceholder.typicode.com'
+
+if __name__ == '__main__':
+
+    USER_ID = sys.argv[1]
+
+    user = requests.get(f'{API_URL}/users/{USER_ID}').json()
+    todo_list = requests.get(f"{API_URL}/todos?userId={USER_ID}").json()
+
+    json_filename = f"{USER_ID}.json"
+
+    data = {
+        USER_ID: [
+            {
+                "task": task['title'],
+                "completed": task['completed'],
+                "username": user['username']
+            }
+            for task in todo_list
+        ]
+    }
+    with open(json_filename, 'w') as json_file:
+        json.dump(data, json_file)
